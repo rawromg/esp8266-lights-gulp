835b78c67d707141678189941033b11fa681defd
Adam Hageman <adamhageman@Adams-MacBook-Air.local>
2017-08-07 22:42:49 -0700

    Sidebar works as it should now.

--- html/js/main.js
+++ html/js/main.js
@@ -8,13 +8,14 @@ $( document ).ready(function(){
     $('.rightmenu').sideNav({
 	menuWidth: '100%',
     edge: 'right', // Choose the horizontal origin
-    closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
+    closeOnClick: false, // Closes side-nav on <a> clicks, useful for Angular/Meteor
     draggable: false, // Choose whether you can drag to open on touch screens,
     });
     //Menu ^^
-    
-    
 });
+
+
+
 
 var connection = new WebSocket('ws://192.168.0.103:81/', ['arduino']);
 	connection.onopen = function () {  connection.send('Connect ' + new Date()); }; 
@@ -27,3 +28,7 @@ var connection = new WebSocket('ws://192.168.0.103:81/', ['arduino']);
 		console.log('Server: ', e.data);
 		Materialize.toast('Websocket Disconnected', 4000)
 	};
+
+
+
+
